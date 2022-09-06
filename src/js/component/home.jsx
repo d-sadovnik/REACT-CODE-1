2f1053c359ebf3b5e9c2d432ca6b0f5c57816f41
d-sadovnik <borixe@gmail.com>
2022-09-06 23:48:02 +0000

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -19,6 +19,7 @@ const Home = () => {
 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
 				love!
 			</p>
+			<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 		</div>
 	);
 };
