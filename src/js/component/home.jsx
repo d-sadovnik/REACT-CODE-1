851f64cbc130af2efffc5f846da4de8ed1dda858
d-sadovnik <borixe@gmail.com>
2022-09-07 01:58:59 +0000

    please commit jumbotron plus card plus navbar changes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,27 +1,34 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import jumbotron from "./jumbotron.jsx";
+import Mycard from "./card.jsx";
+import navbar from "./navbar.jsx";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-			<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-		</div>
-	);
+  return (
+    <div>
+      <div>{navbar()}</div>
+      <div className="container">
+        <div>{jumbotron()}</div>
+        <div className="row">
+          <div class ="col-3">
+        <div>{Mycard()}</div>
+        </div>
+        <div class ="col-3">
+        <div>{Mycard()}</div>
+        </div>
+        <div class ="col-3">
+        <div>{Mycard()}</div>
+        </div>
+        <div class ="col-3">
+        <div>{Mycard()}</div>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -6,9 +6,7 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-// import Home from "./component/home.jsx";
-import Jumbotron from "./component/Jumbotron.jsx";
+import Home from "./component/home.jsx";
 
 //render your react application
-// ReactDOM.render(<Home />, document.querySelector("#app"));
-ReactDOM.render(<Jumbotron />, document.querySelector("#app"));
+ReactDOM.render(<Home />, document.querySelector("#app"));
